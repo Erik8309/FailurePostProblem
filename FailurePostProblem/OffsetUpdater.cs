9a7599cbd177eba2f28024f9c3cb5632fc92174b
Erik Eriksson <erik.eriksson@white.se>
2017-03-21 21:46:26 +0100

    Initial commit

---
+++ FailurePostProblem/OffsetUpdater.cs
@@ -0,0 +1,41 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FailurePostProblem
+{
+    public class OffsetUpdater : IUpdater
+    {
+        private UpdaterId _updaterId;
+        public OffsetUpdater(AddInId addinID)
+        {
+            _updaterId = new UpdaterId(addinID, new Guid("6C00C0B8-DB13-4B05-ADB4-1AF146CF0A64"));
+        }
+
+        public void Execute(UpdaterData data)
+        {
+            var doc = data.GetDocument();
+
+
+
+            Startup.PostErrorFailure(doc, data.GetAddedElementIds());
+
+
+
+        }
+        public string GetAdditionalInformation() => "Updater to lock the offset parameter";
+
+
+
+        public ChangePriority GetChangePriority() => ChangePriority.FreeStandingComponents;
+
+        public UpdaterId GetUpdaterId() => _updaterId;
+
+
+        public string GetUpdaterName() => "Offset locker";
+    
+    }
+}
---
+++ FailurePostProblem/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FailurePostProblem
+{
+    public class Startup : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            RegisterFailure();
+            RegisterUpdater(application.ActiveAddInId);
+            return Result.Succeeded;
+        }
+
+        public static void RegisterUpdater(AddInId addinId)
+        {
+            var updater = new OffsetUpdater(addinId);
+
+            UpdaterRegistry.RegisterUpdater(updater);
+
+            UpdaterRegistry.AddTrigger(updater.GetUpdaterId(), new ElementClassFilter(typeof(FamilyInstance)), Element.GetChangeTypeParameter(new ElementId(BuiltInParameter.INSTANCE_FREE_HOST_OFFSET_PARAM)));
+            UpdaterRegistry.AddTrigger(updater.GetUpdaterId(), new ElementClassFilter(typeof(FamilyInstance)), Element.GetChangeTypeGeometry());
+            UpdaterRegistry.AddTrigger(updater.GetUpdaterId(), new ElementClassFilter(typeof(FamilyInstance)), Element.GetChangeTypeElementAddition());
+        }
+        public static readonly FailureDefinitionId OffsetErrorFailureDefinitionId = new FailureDefinitionId(new Guid("4240E39E-1EFD-4BD1-B66B-1948E003B341"));
+
+        public static void RegisterFailure()
+        {
+            var failure = FailureDefinition.CreateFailureDefinition(OffsetErrorFailureDefinitionId, FailureSeverity.Error, "The offset parameter cannot be changed for this element. Please use W_Elevation.");
+
+
+        }
+        public static void PostErrorFailure(Document doc, ICollection<ElementId> failingElementIds)
+        {
+            var failureMessage = new FailureMessage(OffsetErrorFailureDefinitionId);
+
+            failureMessage.SetFailingElements(failingElementIds);
+
+            doc.PostFailure(failureMessage);
+
+        }
+    }
+}
